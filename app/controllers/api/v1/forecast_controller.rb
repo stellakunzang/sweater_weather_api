6e7ca5294a7f0a72e0a32e10f23008a79ee4d9a0
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-01 16:19:27 -0600

    Call serializer from controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    destination = Destination.new(params[:location])
-    forecast = Forecast.new(destination.latitude, destination.longitude).forecast
-    
+    d = Destination.new(params[:location])
+    forecast = Forecast.new(d.location, d.latitude, d.longitude)
+    render json: ForecastSerializer.new(forecast)
   end
 end
