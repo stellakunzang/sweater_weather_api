843ee8be1e3c0e36a877db9e45f06e184bf58f3d
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-02 15:45:41 -0600

    Add password confirmation validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   before_create :set_api_key
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
+  validates_presence_of :password_confirmation, require: true
 
   has_secure_password
 
