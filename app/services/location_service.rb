1e9d7367af75df8eda981d7e7015ce45786ee27a
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-02 19:18:49 -0600

    Add roadtrip to location service

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -8,6 +8,15 @@ class LocationService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def road_trip(origin, destination)
+    response = conn.get("directions/v2/route") do |req|
+      req.params[:key] = ENV["MAPQUEST_API_KEY"]
+      req.params[:from] = origin
+      req.params[:to] = destination
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def conn
     Faraday.new("http://www.mapquestapi.com/")
   end
