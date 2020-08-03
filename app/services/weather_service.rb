87f7a2973fb3bdb7697a73e87e292da17d162f49
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-03 10:36:08 -0600

    Remove unnecessary method

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -13,17 +13,6 @@ class WeatherService
    JSON.parse(response.body, symbolize_names: true)
   end
 
-  def trip_forecast(lat, lon)
-  response = conn.get("data/2.5/onecall") do |req|
-    req.params[:lat] = lat
-    req.params[:lon] = lon
-    req.params[:exclude] = "minutely,current,daily"
-    req.params[:units] = "imperial"
-    req.params[:appid] = ENV["OPEN_WEATHER_API_KEY"]
-   end
-   JSON.parse(response.body, symbolize_names: true)
-  end
-
   def conn
     Faraday.new("http://api.openweathermap.org/")
   end
