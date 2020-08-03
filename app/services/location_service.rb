ef268822018170c58d9b962c185fffd680eba2f1
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-03 09:57:16 -0600

    Create distance_to_trail method in location service

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -8,6 +8,16 @@ class LocationService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def distance_to_trail(origin_lat, origin_lon, dest_lat, dest_lon)
+    response = conn.get("directions/v2/route") do |req|
+      req.params[:key] = ENV["MAPQUEST_API_KEY"]
+      req.params[:from] = "#{origin_lat},#{origin_lon}"
+      req.params[:to] = "#{dest_lat},#{dest_lon}"
+    end
+    json = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+  end
+
   def road_trip(origin, destination)
     response = conn.get("directions/v2/route") do |req|
       req.params[:key] = ENV["MAPQUEST_API_KEY"]
